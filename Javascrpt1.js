1080ac7480a5dcb8095df0a9893d2d922b2de4c7
OtonyeR <78686910+OtonyeR@users.noreply.github.com>
2022-06-24 00:25:48 +0100

    Update Javascrpt1.js

--- Javascrpt1.js
+++ Javascrpt1.js
@@ -1,3 +1,3 @@
-console.log('My name is Otonye Robinson');
-console.log('I\'m 5\'6 tall');
-console.log('I\'m from Nigeria');
+alert('My name is Otonye Robinson');
+alert('I\'m 5\'6 tall');
+alert('I\'m from Nigeria');
